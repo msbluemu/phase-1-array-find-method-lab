211f342807969136b1a99865ab676bae248be1a1
Qianyun Mu <msbluemu@gmail.com>
2023-05-17 14:46:11 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+     { year: "2015", result: "W"},
+     { year: "2014", result: "N/A"},
+     { year: "2013", result: "L"},
+   ]
+
+   function superbowlWin(record) {
+    const winObject = record.find(obj => obj.result === 'W');
+    return winObject ? winObject.year : undefined;
+  }
+  
+console.log(superbowlWin(record));
